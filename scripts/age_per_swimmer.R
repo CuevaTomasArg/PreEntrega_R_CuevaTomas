f45571e20ab0a5140f65494f5f4aae3cc012278b
Cueva Tomas Santiago <tomassantiagocueva@gmail.com>
2023-01-18 16:30:34 -0300

    bug fixes in knit

--- scripts/age_per_swimmer.R
+++ scripts/age_per_swimmer.R
@@ -2,10 +2,9 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 
-data <- read.csv("./data/best_swimmers.csv",sep = ",")
+data <- read.csv("../data/best_swimmers.csv",sep = ",")
 
-glimpse(data)
-view(data)
+
 data <- data %>% 
   mutate(
     Swim.date = as.Date(Swim.date,format = "%m-%d-%y"),
@@ -14,6 +13,17 @@ data <- data %>%
     Team.Name = as.factor(Team.Name),
     Age.to.swim = trunc(as.numeric((`Swim.date`- `Athlete.birth.date`)/365))
   )
+
+
+data <- data %>% 
+  select(Athlete.Full.Name, Age.to.swim) %>%
+  unique() %>% 
+  group_by(Age.to.swim) %>% 
+  summarise(
+    Amount.Swimmers = sum(Age.to.swim/Age.to.swim)
+  ) %>% 
+  arrange(desc(Amount.Swimmers))
+
 
 glimpse(data)
 
